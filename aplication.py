e34151779981c7e8ee03693dd3b2db858d072db9
JenniferSrichart <jennifer@sanchez-richart.com>
2021-12-14 14:03:57 +0100

    4ta

--- aplication.py
+++ aplication.py
@@ -1,7 +1,7 @@
 from flask import Flask
 # print a nice greeting.
 def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
+    return '<p>Hi %s!</p>\n' % username
 # some bits of text for the page.
 header_text = '''
 <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
